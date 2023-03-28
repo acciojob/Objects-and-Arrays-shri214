458c16acd56b76c297a20846af663b5f0ea86991
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-03-28 12:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const person = {
       name: 'John Doe',
       age: 80
     };
+let team=players;
+let team1=[];
+for (let i = 0; i < players.length; i++) {
+    team1[i]=players[i];
+};
+
+let cap1={};
+for(let key in person){
+	team1[key]=person[key];
+	team1[key]=person[key];
+}
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
